e1be4282a11e744fbeff4523e08df571197daa00
Hardik Patel <113786016+HardikPatel-DX@users.noreply.github.com>
2024-09-16 08:18:49 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,6 +8,21 @@ document.addEventListener("DOMContentLoaded", function() {
     gsap.from("#projects .project", { opacity: 0, y: 20, duration: 1, delay: 2.5, stagger: 0.3 });
     gsap.from("#contact h2", { opacity: 0, y: -50, duration: 1, delay: 3 });
     gsap.from("#contact p", { opacity: 0, y: 20, duration: 1, delay: 3.5 });
+
+    // Strength hover effect
+    const strengths = document.querySelectorAll('.strength');
+    
+    strengths.forEach(strength => {
+        strength.addEventListener('mouseenter', function () {
+            const skillName = this.getAttribute('data-skill');
+            const skillLevel = this.getAttribute('data-level');
+            this.setAttribute('title', `${skillName} (Level: ${skillLevel})`);
+        });
+        
+        strength.addEventListener('mouseleave', function () {
+            this.removeAttribute('title');
+        });
+    });
 });
 
 // Smooth Scroll
